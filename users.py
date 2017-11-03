3c114a770a4c883f82240110d4aff4bd298308b0
sayibet <32174925+sayibet@users.noreply.github.com>
2017-11-04 02:22:33 +0300

    Update users.py

--- users.py
+++ users.py
@@ -1,14 +1,16 @@
 from flask import current_app
-from flask_login import UserMixin
+from passlib.apps import custom_app_context as pwd_context
 
-
-class User(UserMixin):
+class User:
     def __init__(self, name, surname, email, nickname, password):
         self.nickname = nickname
-        self.password = password
+        self.hashed = hashing(password)
         self.name = name
         self.surname = surname
         self.email = email
 
     def get_id(self):
         return self.nickname
+
+def hashing(password):
+    return pwd_context.encrypt(password)
