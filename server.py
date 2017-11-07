055e0f9c9978228bd8b16b0187e36eec44608a4f
gamzeakyol <gamze.akyol.3@gmail.com>
2017-11-08 02:59:32 +0300

    server.py is updated according to added table (HOMEMADE FOOD).

--- server.py
+++ server.py
@@ -4,21 +4,19 @@ from handlers import site
 from products import Product
 from store import Store
 
+import os
+
 def create_app():
     app = Flask(__name__)
     app.config.from_object('settings')
     app.register_blueprint(site)
 
-    app.store = Store()
-    app.store.add_product(Product('Cake'))
-    app.store.add_product(Product('Cookies', quantity = 10))
-
     return app
-
 
 def main():
     app = create_app()
     #debug = app.config['DEBUG']
+    app.store = Store(os.path.join("\\", "products.sql"))
     port = app.config.get('PORT', 8080)
     app.run(host='0.0.0.0', port=port)
 
