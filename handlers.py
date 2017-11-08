37e0b17efa249c7f9033c5a5dfe40f197573c5ae
sayibet <32174925+sayibet@users.noreply.github.com>
2017-11-08 17:50:10 +0300

    redirect from register is fixed

--- handlers.py
+++ handlers.py
@@ -36,4 +36,4 @@ def register():
     #db.session.add(user)
     #db.session.commit()
     #^^flash('Registration is successful!!')
-    return redirect(url_for('site.home_page'))
+    return redirect('/')
