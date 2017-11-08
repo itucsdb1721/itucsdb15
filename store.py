8ef794c2f3526f10a3479bd0c262fe61de5933c7
sayibet <32174925+sayibet@users.noreply.github.com>
2017-11-08 03:26:24 +0300

    Update store.py
    CRUD for user table has added

--- store.py
+++ store.py
@@ -1,5 +1,6 @@
 import psycopg2 as dbapi2
 from products import Product
+from users import User
 
 
 class Store:
@@ -45,3 +46,42 @@ class Store:
             products = [(key, Product(name, quantity, kind, allergens, seller))
                         for key, name, quantity, kind, allergens, seller in cursor]
         return products
+
+    def add_user(self, users):
+        with dbapi2.connect(self.dbfile) as connection:
+            cursor = connection.cursor()
+            query = "INSERT INTO USERS (NAME, SURNAME, NICKNAME, EMAIL, PASSWORD ) VALUES (?, ?, ?, ?, ?)"
+            cursor.execute(query, (users.name, users.surname, users.nickname, users.email, users.password))
+            connection.commit()
+            self.last_key = cursor.lastrowid
+
+    def delete_user(self, key):
+        with dbapi2.connect(self.dbfile) as connection:
+            cursor = connection.cursor()
+            query = "DELETE FROM USERS WHERE (ID = ?)"
+            cursor.execute(query, (key,))
+            connection.commit()
+
+    def update_user(self, key, name, surname, nickname, email, password):
+        with dbapi2.connect(self.dbfile) as connection:
+            cursor = connection.cursor()
+            query = "UPDATE USERS SET NAME = ?,SURNAME = ?, NICKNAME = ?, EMAIL = ?, PASSWORD = ? WHERE (ID = ?)"
+            cursor.execute(query, (name, surname, nickname, email, password, key))
+            connection.commit()
+
+    def get_user(self, key):
+        with dbapi2.connect(self.dbfile) as connection:
+            cursor = connection.cursor()
+            query = "SELECT NAME, SURNAME, NICKNAME, EMAIL, PASSWORD FROM USERS WHERE (ID = ?)"
+            cursor.execute(query, (key,))
+            name, surname, nickname, email, password = cursor.fetchone()
+        return User(name, surname, nickname, email, password)
+
+    def get_users(self):
+        with dbapi2.connect(self.dbfile) as connection:
+            cursor = connection.cursor()
+            query = "SELECT ID, NAME, SURNAME, NICKNAME, EMAIL, PASSWORD FROM USERS ORDER BY ID"
+            cursor.execute(query)
+            users = [(key, User(name, surname, nickname, email, password))
+                      for key, name, surname, nickname, email, password in cursor]
+        return users
